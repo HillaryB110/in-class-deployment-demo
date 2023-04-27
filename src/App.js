85ab23be77331dbf8101b409de68008981d855b5
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-04-27 11:38:26 -0400

    New Slogan

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Hamster Lord!
         </a>
       </header>
     </div>
